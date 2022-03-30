89387fce4683acf33d7bb2571c3db2770bd39703
Marga <margalppalacios@gmail.com>
2022-03-30 13:00:01 +0200

    En caso de lista vacia

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-arr = ["a","b", "c", "d","e"]
+arr = ['a','b']
+
+def errores(lista):
+    if lista  == []: #en el caso de que no haya ningun elemento
+        return None
+
+    # elif lista == : #en el caso que solo haya un solo elemento
+    #     pass
+
+    else:
+        def ultimo_primer_carracter(lista):
+
+               primer_ultimo = []
+               primer_ultimo.append(lista[0])
+               primer_ultimo.append(lista[-1])
+               return primer_ultimo
+
+    return ultimo_primer_carracter(lista)
 
 
-def ultimo_primer_carracter(lista):
-
-    primer_ultimo = []
-
-    primer_ultimo.append(lista[0])
-    primer_ultimo.append(lista[-1])
-
-    return primer_ultimo
-
-
-
-print(ultimo_primer_carracter(arr))
+print(errores(arr))
     
 
