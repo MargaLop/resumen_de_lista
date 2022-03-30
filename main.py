55dbb9831f671b98a5e499c1b314d0c70909ed60
Marga <margalppalacios@gmail.com>
2022-03-30 12:51:18 +0200

    Función de primer y ultimo elemento: RESUELTO

---
+++ main.py
@@ -0,0 +1,17 @@
+arr = ["a","b", "c", "d","e"]
+
+
+def ultimo_primer_carracter(lista):
+
+    primer_ultimo = []
+
+    primer_ultimo.append(lista[0])
+    primer_ultimo.append(lista[-1])
+
+    return primer_ultimo
+
+
+
+print(ultimo_primer_carracter(arr))
+    
+
